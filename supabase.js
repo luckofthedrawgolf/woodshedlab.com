d907aff7dc49c1013894822f123d4f0d2f97fa53
luckofthedrawgolf <cstaus88@gmail.com>
2025-10-24 00:43:35 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://bzdmmfyvufkzusloibad.supabase.co'
-const SUPABASE_ANON_KEY = 'YOUR_ANON_PUBLIC_KEY'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6ZG1tZnl2dWZrenVzbG9pYmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNzMwMzEsImV4cCI6MjA3Njg0OTAzMX0.E_0Ln8YJryJPeQOhC99cVsuNfEEoAuQy63z6T9qLkYc'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
